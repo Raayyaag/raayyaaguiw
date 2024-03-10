4ca142ac2c1e45894ff27dd4c519b14f7133afc7
Raayyaag <raayyaag@gmail.com>
2024-03-10 17:00:30 +0530

    App enhancements

--- src/pages/DemoProduct.js
+++ src/pages/DemoProduct.js
@@ -120,47 +120,47 @@ const DemoProduct = (props) => {
                     <form onSubmit={sendEmail} id="demoProductForm">
                         <div className="w-full bg-white p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl">
                             <div className="flex">
-                                <h1 className="font-bold text-center lg:text-left text-blue-900 uppercase text-4xl">Demo our products</h1>
+                                <h1 className="font-bold text-center lg:text-left text-teal-800 uppercase text-4xl">Demo our products</h1>
                             </div>
                             <div className="flex items-center my-4">
                                 <input 
-                                    id="checkbox-1" 
+                                    id="teal-checkbox" 
                                     aria-describedby="checkbox-1" 
                                     type="checkbox" 
-                                    className="bg-gray-50 border-gray-300 focus:ring-3 focus:ring-blue-300 h-4 w-4 rounded" 
+                                    className="bg-gray-50 border-teal-300 focus:ring-teal-500 focus:ring-teal-300 h-4 w-4 rounded" 
                                     value="business_management_system" onChange={handleChange}
                                  />
-                                <label htmlFor="checkbox-1" className="ml-3 text-lg font-medium text-gray-900">R6Schools</label>
+                                <label htmlFor="checkbox-1" className="ml-3 text-lg font-medium text-teal-600">R6Schools</label>
                             </div>
                             <div className="flex items-center my-4">
                                 <input 
-                                    id="checkbox-1" 
+                                    id="teal-checkbox" 
                                     aria-describedby="checkbox-1" 
                                     type="checkbox" 
-                                    className="bg-gray-50 border-gray-300 focus:ring-3 focus:ring-blue-300 h-4 w-4 rounded"
+                                    className="bg-gray-50 border-teal-300 focus:ring-teal-500 focus:ring-teal-300 h-4 w-4 rounded"
                                     value="school_management_portal" onChange={handleChange}
                                     />
-                                <label htmlFor="checkbox-1" className="ml-3 text-lg font-medium text-gray-900">Chick's Nest</label>
+                                <label htmlFor="checkbox-1" className="ml-3 text-lg font-medium text-teal-600">Chick's Nest</label>
                             </div>
                             <div className="flex items-center my-4">
                                 <input 
-                                    id="checkbox-1" 
+                                    id="teal-checkbox" 
                                     aria-describedby="checkbox-1" 
                                     type="checkbox" 
-                                    className="bg-gray-50 border-gray-300 focus:ring-3 focus:ring-blue-300 h-4 w-4 rounded" 
+                                    className="bg-gray-50 border-teal-300 focus:ring-teal-500 focus:ring-teal-300 h-4 w-4 rounded" 
                                     value="payroll_management_system" onChange={handleChange}
                                 />
-                                <label htmlFor="checkbox-1" className="ml-3 text-lg font-medium text-gray-900">PacNowry</label>
+                                <label htmlFor="checkbox-1" className="ml-3 text-lg font-medium text-teal-600">PacNowry</label>
                             </div>
                             <div className="flex items-center my-4">
                                 <input 
-                                    id="checkbox-1" 
+                                    id="teal-checkbox" 
                                     aria-describedby="checkbox-1" 
                                     type="checkbox" 
-                                    className="bg-gray-50 border-gray-300 focus:ring-3 focus:ring-blue-300 h-4 w-4 rounded"
+                                    className="bg-gray-50 border-teal-300 focus:ring-teal-500 focus:ring-teal-300 h-4 w-4 rounded"
                                     value="event_management_system" onChange={handleChange}
                                 />
-                                <label htmlFor="checkbox-1" className="ml-3 text-lg font-medium text-gray-900">Farm 2 Doorstep</label>
+                                <label htmlFor="checkbox-1" className="ml-3 text-lg font-medium text-teal-600">Farm 2 Doorstep</label>
                             </div>
                             {errors && 
                                 <p className="text-red-500 text-sm">{errors.products}</p>
@@ -170,7 +170,7 @@ const DemoProduct = (props) => {
                                 <div>
                                     <input 
                                         name="first_name" 
-                                        className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                                        className="w-full bg-gray-100 text-teal-600 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                                         type="text" 
                                         placeholder="First Name*" 
                                         value={firstName}
@@ -185,7 +185,7 @@ const DemoProduct = (props) => {
                                 <div>
                                     <input 
                                         name="last_name" 
-                                        className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                                        className="w-full bg-gray-100 text-teal-600 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                                         type="text" 
                                         placeholder="Last Name*"
                                         value={lastName}
@@ -200,7 +200,7 @@ const DemoProduct = (props) => {
                                 <div>
                                     <input 
                                         name="email"
-                                        className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                                        className="w-full bg-gray-100 text-teal-600 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                                         type="email" 
                                         placeholder="Email*"
                                         value={email}
@@ -215,7 +215,7 @@ const DemoProduct = (props) => {
                                 <div>
                                     <input
                                         name="phone_number" 
-                                        className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                                        className="w-full bg-gray-100 text-teal-600 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                                         type="number" 
                                         placeholder="Phone*"
                                         value={phone}
@@ -231,7 +231,7 @@ const DemoProduct = (props) => {
                             <textarea 
                                 name="message" 
                                 placeholder="Message*" 
-                                className="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                                className="w-full h-32 bg-gray-100 text-teal-600 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                                 value={message}
                                 onChange={(e)=> setMessage(e.target.value)}
                                 onKeyUp={clearErrors}
@@ -241,14 +241,14 @@ const DemoProduct = (props) => {
                             }
                         </div>
                         <div className="my-2 w-1/2 lg:w-2/4">
-                            <button type="submit" id="submitBtn" className="uppercase text-sm font-bold tracking-wide bg-gray-500 hover:bg-blue-900 text-gray-100 p-3 rounded-lg w-full 
+                            <button type="submit" id="submitBtn" className="uppercase text-sm font-bold tracking-wide bg-teal-500 hover:bg-emerald-800 text-gray-100 p-3 rounded-lg w-full 
                                     focus:outline-none focus:shadow-outline">
                                 Send Message
                             </button>
                         </div>
                     </div>
                     </form>
-                    <div className="w-full  lg:-mt-96 lg:w-2/6 px-8 py-6 ml-auto bg-blue-900 rounded-2xl">
+                    <div className="w-full  lg:-mt-96 lg:w-2/6 px-8 py-6 ml-auto bg-teal-500 rounded-2xl">
                         <div className="flex flex-col text-white">     
                             <div className="flex my-4 w-2/3 lg:w-3/4">
                                 <div className="flex flex-col">
@@ -256,10 +256,11 @@ const DemoProduct = (props) => {
                                 </div>
                                 <div className="flex flex-col">
                                     <h2 className="text-2xl">Office Address</h2>
-                                    <p className="text-gray-400">Raayyaag,</p>
-                                    <p className="text-gray-400">South Street, </p>
-                                    <p className="text-gray-400">Reguramapuram,</p>
-                                    <p className="text-gray-400">India.</p>
+                                    <p className="text-white-800">Raayyaag,</p>
+                                    <p className="text-white-800">#93, Tirupati Garden, </p>
+                                    <p className="text-white-800">Kovilpatti-628501,</p>
+                                    <p className="text-white-800">TamilNadu,</p>
+                                    <p className="text-white-800">India.</p>
                                 </div>
                             </div>
                             
@@ -270,21 +271,21 @@ const DemoProduct = (props) => {
 
                                 <div className="flex flex-col">
                                     <h2 className="text-2xl">Call Us</h2>
-                                    <p className="text-gray-400">+91 9087459062</p>
+                                    <p className="text-white-800">+91 9087459062</p>
                                 
                                     <div className='mt-5'>
                                         <h2 className="text-2xl">Send an E-mail</h2>
-                                        <p className="text-gray-400">raayyaag@gmail.com</p>
+                                        <p className="text-white-800">raayyaag@gmail.com</p>
                                     </div>
                             
                                 </div>
                             </div>
                             
                             <div className="flex my-4 w-2/3 lg:w-1/2">
-                                <a href="https://www.facebook.com/Raayyaag" target="_blank" rel="noreferrer" className="rounded-full flex justify-center bg-white h-8 text-blue-900  w-8 inline-block mx-1 text-center pt-1">
+                                <a href="https://www.facebook.com/Raayyaag" target="_blank" rel="noreferrer" className="rounded-full flex justify-center bg-white h-8 text-teal-800  w-8 inline-block mx-1 text-center pt-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className='fill-current font-black hover:animate-pulse'><path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path></svg>
                                 </a>
-                                <a href="https://www.instagram.com/raayyaag/" target="_blank" rel="noreferrer" className="rounded-full flex justify-center bg-white h-8 text-blue-900  w-8 inline-block mx-1 text-center pt-1">
+                                <a href="https://www.instagram.com/raayyaag/" target="_blank" rel="noreferrer" className="rounded-full flex justify-center bg-white h-8 text-teal-800  w-8 inline-block mx-1 text-center pt-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className='fill-current font-black hover:animate-pulse'><circle cx="4.983" cy="5.009" r="2.188"></circle><path d="M12 0C8.74 0 8.333.015 7.053.072 5.775.132 4.905.333 4.14.63c-.789.306-1.459.717-2.126 1.384S.935 3.35.63 4.14C.333 4.905.131 5.775.072 7.053.012 8.333 0 8.74 0 12s.015 3.667.072 4.947c.06 1.277.261 2.148.558 2.913.306.788.717 1.459 1.384 2.126.667.666 1.336 1.079 2.126 1.384.766.296 1.636.499 2.913.558C8.333 23.988 8.74 24 12 24s3.667-.015 4.947-.072c1.277-.06 2.148-.262 2.913-.558.788-.306 1.459-.718 2.126-1.384.666-.667 1.079-1.335 1.384-2.126.296-.765.499-1.636.558-2.913.06-1.28.072-1.687.072-4.947s-.015-3.667-.072-4.947c-.06-1.277-.262-2.149-.558-2.913-.306-.789-.718-1.459-1.384-2.126C21.319 1.347 20.651.935 19.86.63c-.765-.297-1.636-.499-2.913-.558C15.667.012 15.26 0 12 0zm0 2.16c3.203 0 3.585.016 4.85.071 1.17.055 1.805.249 2.227.415.562.217.96.477 1.382.896.419.42.679.819.896 1.381.164.422.36 1.057.413 2.227.057 1.266.07 1.646.07 4.85s-.015 3.585-.074 4.85c-.061 1.17-.256 1.805-.421 2.227-.224.562-.479.96-.899 1.382-.419.419-.824.679-1.38.896-.42.164-1.065.36-2.235.413-1.274.057-1.649.07-4.859.07-3.211 0-3.586-.015-4.859-.074-1.171-.061-1.816-.256-2.236-.421-.569-.224-.96-.479-1.379-.899-.421-.419-.69-.824-.9-1.38-.165-.42-.359-1.065-.42-2.235-.045-1.26-.061-1.649-.061-4.844 0-3.196.016-3.586.061-4.861.061-1.17.255-1.814.42-2.234.21-.57.479-.96.9-1.381.419-.419.81-.689 1.379-.898.42-.166 1.051-.361 2.221-.421 1.275-.045 1.65-.06 4.859-.06l.045.03zm0 3.678c-3.405 0-6.162 2.76-6.162 6.162 0 3.405 2.76 6.162 6.162 6.162 3.405 0 6.162-2.76 6.162-6.162 0-3.405-2.76-6.162-6.162-6.162zM12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm7.846-10.405c0 .795-.646 1.44-1.44 1.44-.795 0-1.44-.646-1.44-1.44 0-.794.646-1.439 1.44-1.439.793-.001 1.44.645 1.44 1.439z"></path></svg>
                                 </a>
                             </div>
